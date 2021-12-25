38d128c8db987208c7735da3ba8e6b514f57ce77
flav1o <flaviodanielmartinscosta@gmail.com>
2021-12-25 00:41:42 +0000

    add day2 part2

--- day2/day2.js
+++ day2/day2.js
@@ -1,9 +1,5 @@
 const fs = require('fs')
 
-let horizontal = 0;
-let depth = 0;
-
-
 const lines = fs
     .readFileSync("day2-input.txt", { encoding: 'utf-8' })
     .split('\n')
@@ -12,10 +8,42 @@ const lines = fs
         return { direction, n: +n }
     })
 
+//############### PART 1 ############### 
+{
+
+let horizontal = 0;
+let depth = 0;
+
 for(let i = 0; i < lines.length; i++) {
     if(lines[i].direction == 'forward') horizontal += lines[i].n;
     if(lines[i].direction == 'down') depth += lines[i].n;
     if(lines[i].direction == 'up') depth -= lines[i].n;
 }
-console
+
 console.log(depth * horizontal);
+}
+
+//############### PART 2 ############### 
+{
+
+    let horizontal = 0;
+    let depth = 0;
+    let aim = 0;
+    
+    for(let i = 0; i < lines.length; i++) {
+        if(lines[i].direction == 'forward') {
+            horizontal += lines[i].n;
+            depth += aim * lines[i].n;
+        }
+
+        if(lines[i].direction == 'down') {
+            aim += lines[i].n;
+        }
+    
+        if(lines[i].direction == 'up') {
+            aim -= lines[i].n;
+        }
+    }
+    
+    console.log(depth * horizontal);
+}
