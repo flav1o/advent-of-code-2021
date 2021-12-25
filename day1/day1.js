69461c0ed9dbb654a8381d8fb52058d48a9d2d06
flav1o <flaviodanielmartinscosta@gmail.com>
2021-12-25 00:11:37 +0000

    add first day part 2

--- day1/day1.js
+++ day1/day1.js
@@ -1,15 +1,39 @@
 const fs = require('fs');
+const { listeners } = require('process');
 const line = fs
     .readFileSync("day1-input.txt", { encoding: "utf-8" })
     .split('\n')
     .map((n) => parseInt(n))
 
-let largerThanPrev = 0;
-let prev = line[0];
 
-for(let i = 0; i < line.length; i++) {
-    prev = line[i - 1]
-    if(line[i] > prev) largerThanPrev++
+
+//############### PART 1 ############### 
+{
+    let largerThanPrev = 0;
+    let prev = line[0];
+
+    for (let i = 0; i < line.length; i++) {
+        prev = line[i - 1]
+        if (line[i] > prev) largerThanPrev++
+    }
+
+    console.log(largerThanPrev);
 }
 
-console.log(largerThanPrev);
+//############### PART 2 ############### 
+{
+    let lastLines = 0;
+    let position = 0;
+    let largerThanPrevtotal = 0;
+
+    for (let i = 3; i < line.length; i++) {
+        position = line[i - 2] + line[i - 1] + line[i];
+        lastLines = line[i - 3] + line[i - 2] + line[i - 1];
+
+        if (position > lastLines) {
+            largerThanPrevtotal++
+        }
+    }
+
+    console.log(largerThanPrevtotal)
+}
